4f27c9de1691009cb9b60c31d78214696b8cf823
wvfeders <wvfedders@frontier.com>
2017-12-26 18:24:46 -0500

    it works!

--- isbn_for_web.rb
+++ isbn_for_web.rb
@@ -36,16 +36,59 @@ else
 # #isbn_array = split_text
 end #ends the if statement
 
+
+def isbn10(isbn_array) #defines a function used to check a 10 digit number
+	counter= 0 #establishes a counter
+	total10 = 0 #establishes a check number total
+	10.times do #initiates a loop through the 10 digit number
+		number  = @isbn_array[counter] #pulls number from the isbn array based on its aray location
+		total10 = total10 + (number*(counter+1)) #does multiplication and adds product to total10
+		counter = counter +1 #updates counter
+	end #ends do loop
+
+	#print "The check total is #{total10}.  \n \n" #prints total10
+	if 0 == total10%11 #performs check to see if total10 is evenly divisible by 11 and prints accordingly
+
+	@outcome = "It is a valid 10-digit ISBN number.  \n \n"
+	else
+	@outcome = 	"It is NOT a valid 10-digit ISBN number.  \n \n"	
+	end #ends if statement
+end #ends the isbn10 function
+
+def isbn13(isbn_array) #defines a function to evaluate a 13 dogot number
+ 	counter= 0 #establishes a counter
+ 	total13 = 0#establishes a check total
+ 	multiples_array = [1,3,1,3,1,3,1,3,1,3,1,3,1] #creates array of values to multiply by the 13 digits
+ 	13.times do #initiates a loop through the 13 digit number
+ 		number  = @isbn_array[counter] #pulls a number from the isbn array based on its array location
+ 		multiplier = multiples_array[counter] #pulls a number from the multiples array 
+ 		total13 = total13 + (number * multiplier) #calculates product of digit and multiplier and adds to total13
+ 		counter += 1 #updates counter
+ 	end #ends loop
+	
+# 	print "The check total is #{total13}.  \n \n" #prints the total13 value
+ 	if 0 == total13%10 #evaluates if total13 is evenly divisible by 10 and prints output acdordingly
+ 		@outcome = "It is a valid 13-digit ISBN number.  \n \n"
+ 	else
+ 		@outcome = 	"It is NOT a valid 13-digit ISBN number.  \n \n"
+ 	end #ends if statement
+	
+ end #ends the isbn13 function
+
+
+
+
 @digcount = @isbn_array.count
 # puts "\nYour number has #{isbn_array.count} digits \n \n" #outputs how many digits were entered
 if @digcount == 10 #checks to see if 10 digits are in the number
 	@ok = "This number is an acceptable length \n \n" #if 10 digits accepts the number
-#  	isbn10(isbn_array) #calls the isbn10 function to further evaluate the number
-#  elsif isbn_array.count == 13 #if number is not 10 digits it checks to see if it is 13 digits
+  	isbn10(@isbn_array) #calls the isbn10 function to further evaluate the number
+elsif @digcount == 13 #if number is not 10 digits it checks to see if it is 13 digits
+	@ok = "This number is an acceptable length"
 # # 	print "This number is an acceptable length \n \n" #if 13 digits it accepts the number
-#  	isbn13(isbn_array, test_number)	#calls the isbn13 function to further evaluate the number
-# # else
-# # 	abort ("This number is not of the proper length  \n \n") #if the number does not have either 10 or 13 digits, the program exits and prints message
+  	isbn13(@isbn_array)	#calls the isbn13 function to further evaluate the number
+else 
+	@ok = "This number is NOT the proper length" # 	abort ("This number is not of the proper length  \n \n") #if the number does not have either 10 or 13 digits, the program exits and prints message
 end
 
 
@@ -63,7 +106,7 @@ end
 # 	counter= 0 #establishes a counter
 # 	total10 = 0 #establishes a check number total
 # 	10.times do #initiates a loop through the 10 digit number
-# 		number  = isbn_array[counter] #pulls number from the isbn array based on its aray location
+# 		number  = @isbn_array[counter] #pulls number from the isbn array based on its aray location
 # 		total10 = total10 + (number*(counter+1)) #does multiplication and adds product to total10
 # 		counter = counter +1 #updates counter
 # 	end #ends do loop
@@ -71,9 +114,9 @@ end
 # 	print "The check total is #{total10}.  \n \n" #prints total10
 # 	if 0 == total10%11 #performs check to see if total10 is evenly divisible by 11 and prints accordingly
 
-# 		print "#{isbn_number} is a valid 10-digit ISBN number.  \n \n"
+# 	@outcome = "This is a valid 10-digit ISBN number.  \n \n"
 # 	else
-# 		print "#{isbn_number} is NOT a valid 10-digit ISBN number.  \n \n"	
+# 	@outcome = 	"This is NOT a valid 10-digit ISBN number.  \n \n"	
 # 	end #ends if statement
 # end #ends the isbn10 function
 
